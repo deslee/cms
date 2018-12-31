cb0d6dd6a08b7dded0745ddaa76b6ac873fb7604
Desmond Lee <desmondclee@gmail.com>
2018-12-31 01:23:55 -0600

    refactor classes to pure functions

--- admin-panel/src/components/Editor/FormikTextEditor.tsx
+++ admin-panel/src/components/Editor/FormikTextEditor.tsx
@@ -1,51 +1,41 @@
 import * as React from 'react';
 import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { EditorState, RawDraftContentState, convertFromRaw, ContentState, convertToRaw } from 'draft-js';
+import { RawDraftContentState } from 'draft-js';
 import TextEditor from './Editor';
 import { FieldProps } from 'formik';
 import { Form } from 'semantic-ui-react';
-
-interface State {
-}
 
 interface Props {
     label?: string;
 }
 
-class FormikTextEditor<V> extends React.Component<Props & FieldProps<V>, State> {
-    constructor(props: Props & FieldProps<V>) {
-        super(props);
-    }
+export default <V extends {}>({
+    label, form: { setFieldValue },
+    field: { name: fieldName, value: fieldValue }
+}: Props & FieldProps<V>) => {
+    let rawState: { type: string, value: RawDraftContentState };
+    let initialState: RawDraftContentState | undefined = undefined;
 
-    render() {
-        let rawState: { type: string, value: RawDraftContentState };
-        let initialState: RawDraftContentState | undefined = undefined;
-
-        if (this.props.field.value) {
-            try {
-                rawState = JSON.parse(this.props.field.value)
-                console.log(rawState);
-            } catch (e) {
-                console.error(e);
-            }
-        };
-
-        if (rawState && rawState.type === 'RawDraftContentState') {
-            initialState = rawState.value;
+    if (fieldValue) {
+        try {
+            rawState = JSON.parse(fieldValue)
+        } catch (e) {
         }
-        return (
-            <Form.Field>
-                <label>{this.props.label}</label>
-                <TextEditor
-                    initialState={initialState}
-                    onChange={(rawState) => {
-                        this.props.form.setFieldValue(this.props.field.name, JSON.stringify({ type: 'RawDraftContentState', value: rawState }))
-                    }}
-                />
-            </Form.Field>
+    };
 
-        )
+    if (rawState && rawState.type === 'RawDraftContentState') {
+        initialState = rawState.value;
     }
-}
+    return (
+        <Form.Field>
+            <label>{label}</label>
+            <TextEditor
+                initialState={initialState}
+                onChange={(rawState) => {
+                    setFieldValue(fieldName, JSON.stringify({ type: 'RawDraftContentState', value: rawState }))
+                }}
+            />
+        </Form.Field>
 
-export default FormikTextEditor;
+    )
+}
